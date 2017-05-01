69ccddb365dad875929c633a5f4941d9e44f7197
Michail Mazo <michail.mazo@inovex.de>
2017-05-01 15:36:43 +0200

    started with crud operations

--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -11,12 +11,55 @@ export class UsersComponent implements OnInit {
 
   public users: User[] = [];
 
+  public selectedUser: User = new User();
+
   constructor(private usersService: UsersService) { }
 
   ngOnInit() {
+    this.getAllUsers();
+  }
+
+  public select(user: User) {
+    this.selectedUser = user;
+  }
+
+  public getAllUsers() {
     this.usersService.getList().subscribe(
       (users: User[]) => {
         this.users = users;
+      }, (error: any) => {
+        console.log(error);
+      });
+  }
+
+  public addNewUser() {
+    this.selectedUser = new User();
+  }
+
+  public saveUser() {
+    if (this.selectedUser.id) {
+      this.usersService.updateOne(this.selectedUser).subscribe(
+        (user: User) => {
+          this.selectedUser = user;
+          this.getAllUsers();
+        }, (error: any) => {
+          console.log(error);
+        });
+    } else {
+      this.usersService.createOne(this.selectedUser).subscribe(
+        (user: User) => {
+          this.selectedUser = user;
+          this.getAllUsers();
+        }, (error: any) => {
+          console.log(error);
+        });
+    }
+  }
+
+  public deleteUser() {
+    this.usersService.deleteOne(this.selectedUser.id).subscribe(
+      (user: User) => {
+        this.getAllUsers();
       }, (error: any) => {
         console.log(error);
       });
